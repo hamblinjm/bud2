bd90bc64699fd617d054742269da239aa4d9d48b
Jessica Hamblin <hamblinjm@hotmail.com>
2021-02-26 13:41:07 -0700

    Added phone number api

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,62 +1,58 @@
 
-var key = "f9db8acf7c05571f9822550bbb412c20";
+var emailKey = "f9db8acf7c05571f9822550bbb412c20";
+var numKey = "b3b8e697ed9b35333936edc0adaff2d6";
 
 document.getElementById("verifySubmit").addEventListener
                                 ("click", function(event){
   event.preventDefault();
-  const value = document.getElementById("email").value;
-  if(value === "")
+  const email = document.getElementById("email").value;
+  if(email === "")
     return;
-  console.log(value);
+  console.log(email);
 
-//fetches the current weather conditions
-var url = "https://apilayer.net/api/check?access_key=" + key + "&email=" + value + "&format=1";
+  const phone = document.getElementById("phone").value;
+  if(phone === "")
+    return;
+  console.log(phone);
 
-  fetch(url)
+// url for email verification
+var url = "https://apilayer.net/api/check?access_key=" + emailKey + "&email=" + email + "&format=1";
+var url2 = "http://apilayer.net/api/validate?access_key=" + numKey + "&number=" + phone + "&format=1";
+
+// fetches email verification
+fetch(url)
+  .then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    console.log(json);
+    /*let results = "";
+    results += '<h2>Weather today in ' + json.name + "</h2>";
+    for (let i = 0; i < json.weather.length; i++) {
+        results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
+    }
+    results += '<h2> Feels like: ' + (json.main.feels_like).toFixed(0) + ' &deg;F </h2>';
+    results += '<p>Current Temperature: ' + (json.main.temp).toFixed(0) + "  &deg;F</p>";
+    results += '<p>Wind Speed: ' + (json.wind.speed).toFixed(0) + " mph</p>";
+
+    results += "<p> Description: ";
+    for (let i=0; i < json.weather.length; i++) {
+        results += json.weather[i].description;
+        if (i !== json.weather.length - 1){
+            results += ", ";
+        }
+    }
+    results += "</p>";
+    //console.log(results);
+    document.getElementById("weatherResults").innerHTML = results;
+    document.getElementById("weatherResults").style.fontFamily = "Impact,Charcoal,sans-serif";*/
+  });
+
+  fetch(url2)
     .then(function(response) {
       return response.json();
     }).then(function(json) {
       console.log(json);
-      /*let results = "";
-      results += '<h2>Weather today in ' + json.name + "</h2>";
-      for (let i = 0; i < json.weather.length; i++) {
-	       results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
-      }
-      results += '<h2> Feels like: ' + (json.main.feels_like).toFixed(0) + ' &deg;F </h2>';
-      results += '<p>Current Temperature: ' + (json.main.temp).toFixed(0) + "  &deg;F</p>";
-      results += '<p>Wind Speed: ' + (json.wind.speed).toFixed(0) + " mph</p>";
 
-      results += "<p> Description: ";
-      for (let i=0; i < json.weather.length; i++) {
-	       results += json.weather[i].description;
-	        if (i !== json.weather.length - 1){
-	           results += ", ";
-          }
-      }
-      results += "</p>";
-      //console.log(results);
-      document.getElementById("weatherResults").innerHTML = results;
-      document.getElementById("weatherResults").style.fontFamily = "Impact,Charcoal,sans-serif";*/
     });
-
-    var defaultClient = cloudmersiveConvertApiClient.ApiClient.instance;
-
-    // Configure API key authorization: Apikey
-    var Apikey = defaultClient.authentications['Apikey'];
-    Apikey.apiKey = "b0d8fb10-010e-497f-878c-33a24b666ed2"
-
-    var api = new cloudmersiveConvertApiClient.ConvertDataApi()
-
-    var inputFile = fs.readFileSync("C:\\temp\\input.docx"); // {File} Input file to perform the operation on.
-
-
-    var callback = function(error, data, response) {
-      if (error) {
-        console.error(error);
-      } else {
-        console.log('API called successfully. Returned data: ' + data);
-      }
-    };
-    api.convertDataCsvToJson(Buffer.from(inputFile.buffer), callback);
 
 });
